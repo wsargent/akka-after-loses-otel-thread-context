2e58d9e2c62525776093ead9c75233a578c263d6
Will Sargent <will.sargent@gmail.com>
2024-07-06 12:05:50 -0700

    Add more contexts

--- src/main/scala/org/example/application/Main.scala
+++ src/main/scala/org/example/application/Main.scala
@@ -2,54 +2,94 @@ package org.example.application
 
 import io.opentelemetry.api.GlobalOpenTelemetry
 import io.opentelemetry.api.trace.Span
+import io.opentelemetry.context.Context
+import org.apache.pekko.Done
 import org.apache.pekko.actor.ActorSystem
 import org.slf4j.LoggerFactory
 
+import scala.language.reflectiveCalls
 import scala.concurrent.duration.DurationInt
-import scala.concurrent.{Await, Future}
+import scala.concurrent.{Await, ExecutionContext, Future}
 
 object Main {
   private val logger = LoggerFactory.getLogger(getClass)
+  private val openTelemetry = GlobalOpenTelemetry.get()
+  private val tracer = openTelemetry.getTracer("example")
 
-  private val InstrumentationName: String = "example"
+  // https://tersesystems.com/blog/2024/06/20/executioncontext.parasitic-and-friends/
+  private val opportunisticExecutionContext = (scala.concurrent.ExecutionContext: {def opportunistic: scala.concurrent.ExecutionContextExecutor}).opportunistic
 
   def main(args: Array[String]): Unit = {
-    val openTelemetry = GlobalOpenTelemetry.get()
-    logger.info(s"Using $openTelemetry")
-    val actorSystem = ActorSystem("example")
-    val tracer = openTelemetry.getTracer(InstrumentationName)
+    implicit val actorSystem = ActorSystem("example")
 
-    def traceSync[A](traceName: String)(block: => A): A = {
-      val span = tracer.spanBuilder(traceName).startSpan()
-      assert(span.isRecording, "No-op span, you must run this class with the java agent so it instruments correctly!")
+    implicit val ec = ExecutionContext.global
+    val f = for {
+      _ <- operation("wrapping")
+      _ <- operation("global")
+      _ <- operation("parasitic")
+      _ <- operation("opportunistic")
+      _ <- operation("dispatcher")
+    } yield actorSystem.terminate()
 
-      try {
-        val scope = span.makeCurrent()
-        try {
-          block
-        } finally {
-          scope.close()
-        }
-      } finally {
-        span.end()
-      }
-    }
+    Await.result(f, 30.seconds)
+  }
 
-    val f = traceSync("root") {
+  def operation(mode: String)(implicit actorSystem: ActorSystem): Future[Done] = {
+    traceSync(s"root $mode") {
       val expectedSpan = Span.current()
-      logger.info(s"We expect ${expectedSpan}")
-      org.apache.pekko.pattern.after(1.second) {
+      logger.info(s"mode: We expect ${expectedSpan}")
+      val afterExecutionContext = defineExecutionContext(mode)
+
+      org.apache.pekko.pattern.after(1.second, actorSystem.scheduler) {
         val actualSpan = Span.current()
         Future.successful {
-          if (! expectedSpan.equals(actualSpan)) {
-            logger.error(s"Unexpected $actualSpan")
+          if (!expectedSpan.equals(actualSpan)) {
+            logger.error(s"$mode: Unexpected $actualSpan")
           } else {
-            logger.info(s"Reached delayed with $actualSpan")
+            logger.info(s"$mode: Reached delayed with $actualSpan")
           }
-          actorSystem.terminate()
+          Done
         }
-      }(actorSystem)
+      }(afterExecutionContext)
     }
-    Await.result(f, 5.seconds)
   }
+
+  def traceSync[A](traceName: String)(block: => A): A = {
+    val span = tracer.spanBuilder(traceName).startSpan()
+    assert(span.isRecording, "No-op span, you must run this class with the java agent so it instruments correctly!")
+
+    try {
+      val scope = span.makeCurrent()
+      try {
+        block
+      } finally {
+        scope.close()
+      }
+    } finally {
+      span.end()
+    }
+  }
+
+  def defineExecutionContext(mode: String)(implicit system: ActorSystem): ExecutionContext = {
+    val dispatcher = system.classicSystem.dispatcher
+
+    mode match {
+      case "wrapping" =>
+        val context = Context.current()
+        new ExecutionContext {
+          override def execute(runnable: Runnable): Unit = dispatcher.execute(context.wrap(runnable))
+
+          override def reportFailure(cause: Throwable): Unit = dispatcher.reportFailure(cause)
+        }
+      case "global" =>
+        ExecutionContext.global
+      case "parasitic" =>
+        ExecutionContext.parasitic
+      case "opportunistic" =>
+        opportunisticExecutionContext
+      case _ =>
+        dispatcher
+    }
+  }
+
 }
